63dbe235c125553f4b16181076a00bb41de387ed
KanakPitalia <kanakpitalia16012001@gmail.com>
2022-01-14 17:12:40 +0530

    added date,and some more weather images

--- Weather/script.js
+++ Weather/script.js
@@ -9,12 +9,12 @@ const weatherApi = {
 var input = document.getElementById('city');
 var weathershow = document.getElementById('weather-show');
 
-// weathershow.style.display = 'none';
+weathershow.style.display = 'none';
 input.addEventListener('keypress', function (event) {
     if (event.key == "Enter") {
         console.log(input.value);
         getweatherDetails(input.value);
-        // weathershow.style.display = 'block';
+
     }
 });
 
@@ -22,8 +22,12 @@ function getweatherDetails(city) {
     // console.log("in the getweatherDetails function");
     fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`).then((weather) => {
         // console.log("in the then function");
+        weathershow.style.display = 'block';
         return weather.json();
-    }).then(showWeatherDetails)
+    }).then(showWeatherDetails).catch((error) => {
+        weathershow.innerHTML = `<p style="color:red;font-family:sans-serif">Sorry,city '${input.value.charAt(0).toUpperCase() + input.value.slice(1)}' Not in the database</p>`
+        document.body.style = `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ),url(img/large/default.jpg)`;
+    })
 }
 function showWeatherDetails(weather) {
     console.log(weather);
@@ -34,7 +38,7 @@ function showWeatherDetails(weather) {
     let climate = document.getElementById('weather-name');
     let body = document.getElementById('body');
     let img = document.getElementById('img');
-    // let cityDate = document.getElementById('city-date')
+    let cityDate = document.getElementById('city-date')
 
     city.innerText = `${weather.name},${weather.sys.country}`;
     temp.innerText = `${Math.round(weather.main.temp)}`;
@@ -42,16 +46,24 @@ function showWeatherDetails(weather) {
     mintemp.innerText = `${Math.floor(weather.main.temp_min)}`;
     climate.innerText = `${weather.weather[0].main}`;
     body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ),url(img/large/${weather.weather[0].main.toLowerCase()}.jpg)`;
-    img.src = `img/small/${weather.weather[0].main.toLowerCase()}.png`;
-    // cityDate.innerText = `${calcDate(weather).getDay(), calcDate(weather).getFullYear()}`;
+    // img.src = `img/small/${weather.weather[0].main.toLowerCase()}.png`;
+    img.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+    let date = new Date();
+    // let utc = date.getTime();
+    // let nd = new Date(utc + 3600000);
+    // let s = date.valueOf();
+    // console.log(s);
+    // let p = s - 19800;
+    // console.log(p);
+    cityDate.innerText = calcDate(date);
 }
 
-// function calcDate(weather) {
-//     var date_in_milli = weather.dt;
-//     console.log(date_in_milli.Date);
-
-//     var date = new Date(date_in_milli);
-//     dateString = date.toUTCString();
-//     console.log(dateString);
-//     return date;
-// }
+function calcDate(date) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let year = date.getFullYear();
+    let month = months[date.getMonth()];
+    let dayNo = date.getDate();
+    let day = days[date.getDay()];
+    return `${day} ${dayNo} ${month},${year}`;
+}
